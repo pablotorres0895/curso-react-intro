54833cee91724e921482a2f7db1af239f28ba744
Pablo Peralta <pablotorres0895@hotmail.com>
2024-11-29 13:01:38 -0600

    Uso de variables state y funcionamiento de barra de busqueda

--- src/App.js
+++ src/App.js
@@ -5,22 +5,36 @@ import { TodoItem } from './TodoItem'
 import { CreateTodoButton } from './CreateTodoButton'
 import React from 'react';
 import './App.css'
+
 const defaultTodos = [
   { text:'Peinar Gatos', completed: true},
   { text:'Dar de comer a los Gatos', completed: false},
   { text:'Jugar Fulbo', completed: false},
-  { text:'Ver tutoriales', completed: true}
+  { text:'Ver tutoriales', completed: true},
+  { text:'Ver tutoriales 2', completed: true}
 ]
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  console.log('Los usuarios buscan todos de ' + searchValue)
+
   return (
     <div className='todo-container'>
       
-      <TodoCounter completed={16} total={20} />
-      <TodoSearch />
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      
+      />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {todos.map(todo => (
           <TodoItem 
             key={todo.text} 
             text={todo.text}
